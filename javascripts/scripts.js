2e43eb131430488f70740f6c5cedda5d27b910e8
jpark1120 <parkj1120@gmail.com>
2014-07-24 18:47:39 -0400

    functioning nav and fadeIn of contents

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,8 +1,32 @@
 function setEventHandlers(){
-  
+
+  $('.links').on('click', function(evt){
+    evt.preventDefault();
+    $('.links').each(function(i, link){
+      $(link).removeClass('clicked');
+    });
+    $(this).addClass("clicked");
+  });
 
 
-  
+  $('#about_me').on('click', function(evt){
+    evt.preventDefault();
+    $('div.about_me').siblings().css('display', 'none');
+    $('.about_me').fadeIn(500);
+  });
+
+  $('#my_work').on('click', function(evt){
+    evt.preventDefault();
+    $('div.my_work').siblings().css('display', 'none');
+    $('.my_work').fadeIn(500);
+  });
+
+  $('#contact_me').on('click', function(evt){
+    evt.preventDefault();
+    $('div.contact_me').siblings().css('display', 'none');
+    $('.contact_me').fadeIn(500);
+  });
+
 }
 
 
