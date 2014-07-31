14a18d5cd8a1b22a26263b910a073f838f801c52
jpark1120 <parkj1120@gmail.com>
2014-07-31 14:44:07 -0400

    changed font and layout of the page and adjusted scripts to account for changes

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -10,33 +10,27 @@ function setEventHandlers(){
 
   $('#about-me').on('click', function(evt){
     evt.preventDefault();
-    $('div.about-me').siblings().hide();
-    $('.about-me').fadeIn(500);
+    $('html, body').animate({
+      scrollTop: ($('.about-me').offset().top)
+    },500);
   });
 
   $('#my-work').on('click', function(evt){
     evt.preventDefault();
-    $('div.my-work').siblings().hide();
-    $('.my-work').fadeIn(500);
+    $('html, body').animate({
+      scrollTop: ($('.my-work').offset().top)
+    },500);
   });
 
   $('#contact-me').on('click', function(evt){
     evt.preventDefault();
-    $('div.contact-me').siblings().hide();
-    $('.contact-me').fadeIn(500);
+    $('html, body').animate({
+      scrollTop: ($('.contact-me').offset().top)
+    },500);
   });
-
-  $('#name-banner').on('click', function(evt){
-    evt.preventDefault();
-    $('div.about-me').siblings().hide();
-    $('.links').each(function(i, link){
-      $(link).removeClass('clicked');
-    });
-    $('#about-me').addClass("clicked");
-    $('.about-me').fadeIn(500);
-  });
-
 }
+// The .offset() method allows us to retrieve the current position of an element relative to the document
+
 
 
 $(function(){
